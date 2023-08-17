db9f2ad55052a06f26f76af224f3f2cc17d1d511
SlingShot121 <lightningnite843@gmail.com>
2023-08-17 07:12:48 +0200

    1

--- script.js
+++ script.js
@@ -1,68 +1,41 @@
-document.getElementById('camera-menu').addEventListener('click', function () {
-    document.getElementById('myModal').style.display = 'block';
-});
+// A function that parses URL parameters
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
 
-document.getElementById('closeModal').addEventListener('click', function () {
-    document.getElementById('myModal').style.display = 'none';
-});
-
-document.getElementById('go-button').addEventListener('click', function () {
-    // Get the user input from the ref-search field
-    let userInput = document.getElementById('ref-search').value;
-    if (userInput) {
-        // Set the user input as the content of the top p tag with id order-ref
-        document.getElementById('outbay').querySelector('#outbay-order-ref').textContent = userInput;
-    }
-
-    // Continue with the rest of your functionality for the "Go" button
-    let outbay2Element = document.getElementById('outbay-2');
-    let outbayElement = document.getElementById('outbay');
-    if (outbay2Element) outbay2Element.style.display = 'inline-flex';
-    if (outbayElement) outbayElement.style.display = 'inline-flex';
-});
-
-document.getElementById('add-button').addEventListener('click', function () {
-    // Check if any of the bottom content is already displayed
-    for (let i = 47; i <= 57; i++) {
-        let element = document.querySelector('.div-div-' + i);
-        if (element && element.style.display === 'inline-flex') {
-            alert('There is already an order in this bay.');
-            return; // Exit the function without making changes
-        }
-    }
-
-    // Get the content from the top p tag with id order-ref
-    let orderRefTop = document.getElementById('outbay-order-ref').textContent;
-
-    // Copy the content to the bottom p tags with id order-ref
-    let bottomOrderRefs = document.querySelectorAll('.div-div-47 #order-ref, .div-div-57 #order-ref');
-    bottomOrderRefs.forEach(function(element) {
-        element.textContent = orderRefTop;
+// When scan is successful function will produce data
+function onScanSuccess(qrCodeMessage) {
+    html5QrCodeScanner.clear().then(() => {
+        window.location.href = qrCodeMessage + "&ref=" + currentReference;
+    }).catch((err) => {
+        console.log(err);
     });
+}
 
-    // Continue with the rest of the functionality
-    let outbay2Element = document.getElementById('outbay-2');
-    let outbayElement = document.getElementById('outbay');
-    if (outbay2Element) outbay2Element.style.display = 'none';
-    if (outbayElement) outbayElement.style.display = 'none';
+// When scan is unsuccessful function will produce error message
+function onScanError(errorMessage) {
+    console.log(errorMessage);
+}
 
-    for (let i = 47; i <= 57; i++) {
-        let element = document.querySelector('.div-div-' + i);
-        if (element) element.style.display = 'inline-flex'; // Show the element
-    }
-    let bayFooterElement = document.getElementById('bay-footer');
-    let bayCapElement = document.getElementById('bay-cap');
-    if (bayFooterElement) bayFooterElement.style.display = 'inline-flex'; // Show the bay-footer element
-    if (bayCapElement) bayCapElement.style.display = 'inline-flex'; // Show the bay-footer element
-});
+// Setting up Qr Scanner properties
+var html5QrCodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 }, /* verbose= */ false, /* disableFlip= */ true);
 
-document.getElementById('remove-button').addEventListener('click', function () {
-    for (let i = 47; i <= 57; i++) {
-        let element = document.querySelector('.div-div-' + i);
-        if (element) element.style.display = 'none';
-    }
-    let bayFooterElement = document.getElementById('bay-footer');
-    let bayCapElement = document.getElementById('bay-cap');
-    if (bayFooterElement) bayFooterElement.style.display = 'none';
-    if (bayCapElement) bayCapElement.style.display = 'none';
-});
+// Usage
+window.onload = function () {
+    var bay = getUrlParameter('bay');
+    var ref = getUrlParameter('ref');
+    var selectElement = document.getElementById('bays-id-503891');
+    var inputElement = document.getElementById('reference-input');
+    var currentReference = "2";
+
+    setSelectedOption(selectElement, bay);
+    inputElement.value = referenceValues[ref];
+
+    // Open the camera app when the button is clicked
+    document.getElementById('scan-bay-button').addEventListener('click', function() {
+        html5QrCodeScanner.render(onScanSuccess, onScanError);
+    });
+}
