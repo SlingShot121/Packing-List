d3df6f6e8dce58f8868ee13984e54d5830136161
SlingShot121 <lightningnite843@gmail.com>
2023-08-21 07:08:54 +0200

    1

--- script.js
+++ script.js
@@ -42,3 +42,34 @@ document.getElementById('scan-bay-button').addEventListener('click', function ()
 
 // Add a click event listener to the close button to toggle the modal
 document.getElementById('close-qr-modal').addEventListener('click', toggleQRModal);
+
+function selectBackFacingCamera() {
+    // Get the select element
+    var selectElement = document.getElementById('html5-qrcode-select-camera');
+
+    // Enumerate the media devices
+    navigator.mediaDevices.enumerateDevices().then(function(devices) {
+        var backFacingCameraIndex = -1;
+
+        // Look for a device labeled as back-facing
+        devices.forEach(function(device, index) {
+            if (device.kind === 'videoinput' && (device.label.toLowerCase().includes('back') || device.label.toLowerCase().includes('rear'))) {
+                backFacingCameraIndex = index;
+            }
+        });
+
+        if (backFacingCameraIndex !== -1) {
+            // Select the back-facing camera by index
+            selectElement.selectedIndex = backFacingCameraIndex;
+
+            // Optionally, trigger a change event if the library requires it
+            var event = new Event('change', { 'bubbles': true });
+            selectElement.dispatchEvent(event);
+        }
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+// Call the function to select the back-facing camera
+selectBackFacingCamera();
